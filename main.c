ddb6706811b6d4f538551951dfe69766126dd0cd
PaulChimzy <paulchimzy@gmail.com>
2023-04-23 17:08:30 +0200

    Creating .c and .h files

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - prints whatever is passed as a variable argument list
+ * @format: pointer to the string that should be printed including (conversion specifiers)  
+ * Return: the number of characters printed if everything works fine
+ */
